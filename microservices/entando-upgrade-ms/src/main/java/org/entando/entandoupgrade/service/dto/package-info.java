5ed181e42d61cc114d22346a115c2174172e23fb
Giovanni Prete <g.prete@entando.com>
2022-10-18 10:43:32 +0200

    Initial version of EntandoUpgrade generated by generator-jhipster@7.2.0

---
+++ microservices/entando-upgrade-ms/src/main/java/org/entando/entandoupgrade/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.entando.entandoupgrade.service.dto;
---
+++ microservices/entando-upgrade-ms/src/main/java/org/entando/entandoupgrade/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.entando.entandoupgrade.web.rest.vm;
